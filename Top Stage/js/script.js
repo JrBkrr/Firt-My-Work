fd7e2c6ce5955291e742cca9c80245110f924cf6
Enes Bakır <enesbkr08@hotmail.com>
2023-03-19 00:35:44 +0300

    Top Stage New Update

--- Top Stage/js/script.js
+++ Top Stage/js/script.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-    $(".nav-links").addClass("show");
-  });
+window.addEventListener("scroll", function() {
+
+  var navbar = document.querySelector(".nav-container");
+  
+  if (window.scrollY > 0) {
+    navbar.classList.add("scroll-hover");
+    console.log(Math.floor(window.scrollY));
+  } else {
+    navbar.classList.remove("scroll-hover");
+  }
+
+});
