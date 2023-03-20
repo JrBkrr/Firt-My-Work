12948b6b545529bdc2a67b9c0969882906788bfd
Enes Bakır <enesbkr08@hotmail.com>
2023-03-21 01:17:04 +0300

    Javascript - update! | css and html - update!

--- New Site/js/script.js
+++ New Site/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('scroll', function() {
+    console.log("'shrink' is working")
+
     var navbar = document.querySelector('.nav-menu');
     navbar.classList.toggle('shrink', window.scrollY > 0);
   });
